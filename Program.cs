8c52f8d46ae6de4bf836c0e96cbf4ae52dd8ca2f
Nolan Pawelski <32725534+pawnol@users.noreply.github.com>
2022-01-12 10:36:31 -0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿/*
+ * For Loop Debug 
+ * Pawelski
+ * 1/121/2022
+ * This program does not work as intended. Using your debugging skills,
+ * find and fix all the bugs so the program works as intended. Record 
+ * your changes on the code provided on the activity sheet.
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForLoopDebug
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i > 10; i++)
+            {
+                Console.WriteLine("All work and no play makes Johnny a dull boy...");
+            }
+        }
+    }
+}
